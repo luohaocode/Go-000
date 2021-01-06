03405689b96ec690f76db784f2901a20bf1787e4
luohaocode <luohaocode@gmail.com>
2021-01-06 19:00:14 +0800

    sliding window counter

--- Week06/sliding_window_counter.go
+++ Week06/sliding_window_counter.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -13,87 +12,104 @@ type Bucket struct {
 }
 
 type Window struct {
-	buckets []*Bucket
-	mu      sync.RWMutex
+	buckets        []*Bucket
+	BucketDuration time.Duration
+	WinDuration    time.Duration
+	mu             sync.RWMutex
 }
 
-func NewWindow(size int) *Window {
-	return &Window{buckets: make([]*Bucket, 0, size)}
-}
-
-func countRequest(buckets []*Bucket) (count int) {
-	for _, b := range buckets {
-		count += b.count
-	}
-	return
+func NewWindow(size int, bucketDuration time.Duration, winDuration time.Duration) *Window {
+	return &Window{buckets: make([]*Bucket, 0, size), BucketDuration: bucketDuration, WinDuration: winDuration}
 }
 
 type SlidingWindowCounter struct {
-	SlotDuration time.Duration
-	WinDuration  time.Duration
-	window       *Window
-	maxRequests  int
+	window      *Window
+	maxRequests int
 }
 
-func NewSlidingWindowCounter(slotDuration time.Duration, winDuration time.Duration, maxRequests int) *SlidingWindowCounter {
+func NewSlidingWindowCounter(bucketDuration time.Duration, winDuration time.Duration, maxRequests int) *SlidingWindowCounter {
 	return &SlidingWindowCounter{
-		SlotDuration: slotDuration,
-		WinDuration:  winDuration,
-		window:       NewWindow(int(winDuration / slotDuration)),
-		maxRequests:  maxRequests,
+		window:      NewWindow(int(winDuration/bucketDuration), bucketDuration, winDuration),
+		maxRequests: maxRequests,
 	}
 }
 
-func (l *SlidingWindowCounter) Run() error {
-	l.window.mu.Lock()
-	defer l.window.mu.Unlock()
+func (c *SlidingWindowCounter) Run() error {
 
+	if c.Sum() >= c.maxRequests {
+		return fmt.Errorf("too many request")
+	}
+
+	c.Increment()
+
+	return nil
+}
+
+func (c *SlidingWindowCounter) getCurrentBucket() *Bucket {
+	now := time.Now()
+	var lastSlot *Bucket
+	if len(c.window.buckets) == 0 {
+		lastSlot = &Bucket{timestamp: now, count: 0}
+		c.window.buckets = append(c.window.buckets, lastSlot)
+	}
+
+	lastSlot = c.window.buckets[len(c.window.buckets)-1]
+	if lastSlot.timestamp.Add(c.window.BucketDuration).Before(now) || lastSlot.timestamp.Add(c.window.BucketDuration).Equal(now) {
+		lastSlot = &Bucket{timestamp: now, count: 0}
+		c.window.buckets = append(c.window.buckets, lastSlot)
+	}
+
+	return lastSlot
+}
+
+func (c *SlidingWindowCounter) RemoveOldBucket() {
 	now := time.Now()
 	timeoutOffset := -1
-	for i, ts := range l.window.buckets {
-		if ts.timestamp.Add(l.WinDuration).After(now) {
+	for i, b := range c.window.buckets {
+		if b.timestamp.Add(c.window.WinDuration).After(now) || b.timestamp.Add(c.window.WinDuration).Equal(now) {
 			break
 		}
 		timeoutOffset = i
 	}
 	if timeoutOffset > -1 {
-		l.window.buckets = l.window.buckets[timeoutOffset+1:]
+		c.window.buckets = c.window.buckets[timeoutOffset+1:]
 	}
+}
 
-	if countRequest(l.window.buckets) >= l.maxRequests {
-		return fmt.Errorf("too many request")
+func (c *SlidingWindowCounter) Increment() {
+	c.window.mu.Lock()
+	defer c.window.mu.Unlock()
+
+	b := c.getCurrentBucket()
+	b.count++
+
+	c.RemoveOldBucket()
+}
+
+func (c *SlidingWindowCounter) Sum() int {
+	c.window.mu.RLock()
+	defer c.window.mu.RUnlock()
+	count := 0
+	for _, b := range c.window.buckets {
+		count += b.count
 	}
+	return count
+}
 
-	var lastSlot *Bucket
-	if len(l.window.buckets) > 0 {
-		lastSlot = l.window.buckets[len(l.window.buckets)-1]
-		if lastSlot.timestamp.Add(l.SlotDuration).Before(now) {
-			lastSlot = &Bucket{timestamp: now, count: 1}
-			l.window.buckets = append(l.window.buckets, lastSlot)
-		} else {
-			lastSlot.count++
-		}
-	} else {
-		lastSlot = &Bucket{timestamp: now, count: 1}
-		l.window.buckets = append(l.window.buckets, lastSlot)
-	}
-
-	return nil
+func (c *SlidingWindowCounter) Avg() int {
+	return c.Sum() / (int(c.window.WinDuration / c.window.BucketDuration))
 }
 
 func main() {
 	counter := NewSlidingWindowCounter(10*time.Millisecond, time.Second, 10)
 
-	var count int32
 	for i := 0; i < 20; i++ {
 		go func() {
-			if err := counter.Run(); err == nil {
-				atomic.AddInt32(&count, 1)
-			}
+			counter.Run()
 		}()
 	}
 
 	time.Sleep(time.Second)
 
-	fmt.Println(count)
+	fmt.Println(counter.Sum())
 }
